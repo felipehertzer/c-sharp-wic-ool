b40452dd1ba26f32b3dc47bde881960695537b9e
Felipe Hertzer <felipeaugustohertzer@gmail.com>
2020-07-21 18:52:21 +1000

    added test

---
+++ Assessment1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Assessment1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Assessment1.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("23221cdf-901a-4a79-917e-4cc527e70360")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Assessment1.Test/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Assessment1.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Assessment1.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestWage()
+        {
+            decimal expected = Decimal.Parse("450");
+            Employee employee = new Employee();
+            employee.EmployeeID = 1;
+            employee.RatePerHour = Decimal.Parse("45");
+
+            decimal actual = employee.CalculateWage(10);
+            decimal diff = 0.0001M;
+
+            Assert.IsTrue(Math.Abs(expected - actual) < diff);
+        }
+
+        [TestMethod]
+        public void TestTax()
+        {
+            decimal expected = Decimal.Parse("86.4");
+            Employee employee = new Employee();
+            employee.EmployeeID = 1;
+            employee.RatePerHour = Decimal.Parse("45");
+
+            decimal actual = employee.CalculateTax(10);
+            decimal diff = 0.0001M;
+
+            Assert.IsTrue(Math.Abs(expected - actual) < diff);
+        }
+    }
+}
